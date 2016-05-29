3295769d3c6a424b15dd8625303ce2db92eeb2fa
Sholes <shirley.kotian@gmail.com>
2016-05-29 15:34:24 +0530

    Add files via upload

---
+++ BirthdayParadox.py
@@ -0,0 +1,17 @@
+from random import randint
+
+count = 0
+w, h = 23, 100000
+
+for x in range(0,h):						#loop for each set of 23 people
+	a = [randint(1,365) for x in range(w)] 			#assign value to set
+	flags=[0 for x in range(366)]				#flags set of 0(false) initially
+
+	for i in range(0,w):					#check for repeated birthdays
+		if flags[a[i]]!=0:
+			count+=1				#count for total number sets in which repitated birthdays occur
+			break					#if atleast one match is found, stop the loop
+		else:
+			flags[a[i]] = 1
+
+print ((float(count)/h)*100)  					#display the percentage of sets where match is found
